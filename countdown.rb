2d521582c1fe7ae22c98a245af1c86c97bf80871
Tabatha Roman <romantabatha17@gmail.com>
2020-09-09 21:18:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,15 @@ num = 12
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
-    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  num = 12
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
   end
   "HAPPY NEW YEAR!"
 end
